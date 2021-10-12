bf2dee1026bb1a734e39ea70e063e85d76a0c5d8
shaharyarsheikh <shaharyarsheikh@folio3.com>
2021-10-12 10:58:47 +0500

    authentication via jwt

---
+++ modules/utils/crypto.js
@@ -0,0 +1,13 @@
+import { AES, enc } from "crypto-js";
+const ENCRYPTION_KEY = "keyhere";
+
+const { encrypt, decrypt } = AES;
+
+export const encrypt_value = (value) => {
+  let v = JSON.stringify(value);
+  return encrypt(v, ENCRYPTION_KEY).toString();
+};
+export const decrypt_value = (value) => {
+  let bytes = decrypt(value.toString(), ENCRYPTION_KEY);
+  return bytes.toString(enc.Utf8);
+};
