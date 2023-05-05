3d9c8c726dba0a7323bbd432455fafa5c714da8c
Marib SuItan <i.maribsultan@gmail.com>
2023-05-05 15:32:29 +0500

    Added primary key in user model

--- src/modules/user/models/user.ts
+++ src/modules/user/models/user.ts
@@ -4,6 +4,13 @@ import { HashService } from "@/modules/common/services";
 @Table({ tableName: "users" })
 export class User extends Model {
 	@Column({
+		type: DataType.BIGINT,
+		allowNull: false,
+		primaryKey: true,
+	})
+	public override id: number;
+
+	@Column({
 		type: DataType.STRING(50),
 		allowNull: false,
 	})
